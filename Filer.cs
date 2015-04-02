c053d99cf186ab2838633b7bec7df92c8d1ed758
poyarkov95 <poyarkov.95@mail.ru>
2015-04-02 16:39:58 +0300

    update

---
+++ Filer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FactoryNEW
+{
+    abstract class Filer
+    {
+        abstract public int rdInt(int count);
+        abstract public double rdDouble(int count);
+        abstract public string rdString(int count);
+        abstract public bool eof();
+        // GIT SDELAI UPDATE POJALSTA
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace FactoryNEW
+{
+    interface Shape
+    {
+        void Load(Filer type);
+        
+    }
+}
